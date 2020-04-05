dfee958c0fedb9d596d85630861304efc6dd20cc
Nils Müller <shimst3r@gmail.com>
2020-04-05 18:36:55 +0200

    WIP: create_venv.py (next step: tests)

---
+++ tests/test_create_venv.py
@@ -0,0 +1,9 @@
+"""
+Test suite for toolbag/create_venv.py
+
+TODO: Test that the function really creates a virtual environment
+TODO: Test the venv_name is set properly
+TODO: Test the prompt_prefix default works as expected
+TODO: Test the prompt_prefix is set properly
+TODO: Test that pip is updated as expected (how to do this with a moving pip target? :thinking:)
+"""
--- toolbag/create_venv.py
+++ toolbag/create_venv.py
@@ -2,21 +2,36 @@
 Creates an up-to-date virtual environment.
 """
 import subprocess
-from typing import Optional
+import sys
+
+import typer
 
 
-def create_venv(venv_dir: str = "venv", prompt_prefix: Optional[str] = None):
+def create_venv(
+    venv_name: str = typer.Option(
+        "venv", "--venv", help="Name of the new venv folder.", show_default=True
+    ),
+    prompt_prefix: str = typer.Option(
+        None, "--prompt", help="Prompt prefix shown when the venv is activated."
+    ),
+):
     """
-    Create a new virtual environment.
+    Create a new virtual environment in the $PWD.
 
-    Create a new virtual environment using the standard library `venv` module in
-    directory `venv_dir` with the possibility to give it an alternative prompt
-    prefix `prompt_prefix`.
+    Create a new virtual environment in the current directiry using the standard
+    library `venv` module in directory `venv_name` with the possibility to give
+    it an alternative prompt prefix `prompt_prefix`.
 
-    Parameters
-    ----------
-    venv_dir : str
-        The directory to create the environment in (default is "venv").
-    prompt_prefix : str, optional
-        An alternative prompt prefix for this environment (default is `venv_dir`).
+    After creating the virtual environment, its pip dependency is updated.
     """
+    if not prompt_prefix:
+        prompt_prefix = venv_name
+
+    subprocess.run([sys.executable, "-m", "venv", venv_name, "--prompt", prompt_prefix])
+
+    venv_executable = f"{venv_name}/bin/python"
+    subprocess.run([venv_executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+
+if __name__ == "__main__":
+    create_venv()
